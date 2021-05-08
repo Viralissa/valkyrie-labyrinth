80316a8da9ffb95121ff2354c22e6fdfbb9e9465
SinScorpion <sinscorpion90@gmail.com>
2021-05-08 18:17:50 +0700

    Labirints
    proba

---
+++ Assets/LabyrinthGenerator/Labyrinth.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+namespace LabyrinthGenerator
+{
+    /// <summary>
+    /// Add this script on Game Object
+    /// </summary>
+    public class Labyrinth : MonoBehaviour
+    {
+
+
+        [SerializeField] private GameObject _cellPrefub;
+        [SerializeField] private GameObject _wallPrefub;
+        [SerializeField] private int _labyrinthSizeX;
+        [SerializeField] private int _labyrinthSizeZ;
+
+
+        private void Start()
+        {
+
+            LabyrinthGenerator labyrinth = new LabyrinthGenerator(
+                transform,
+                _labyrinthSizeX,
+                _labyrinthSizeZ,
+                _cellPrefub,
+                _wallPrefub
+                );
+
+            labyrinth.GenerateLabyrinth();
+        }
+
+
+    }
+}
