66eb4dbf17f5d3dca8fb122e661626da605b5c62
Jack Rayder <jrayder@mail.ru>
2021-05-09 12:21:02 +0300

    Keys

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -3,128 +3,134 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Inventory : MonoBehaviour
+namespace UnityStandardAssets.Characters.FirstPerson
 {
 
-    List<Item> item;
-    public int Range;
-    public GameObject cellContainer;
-    public KeyCode showInventory;
-
-    void Start()
-    {
-        item = new List<Item>();
-
-        cellContainer.SetActive(false);
-
-        for (int i = 0; i < cellContainer.transform.childCount; i++)
-        {
-            item.Add(new Item());
-        }
-    }
-
-    void Update()
+    public class Inventory : MonoBehaviour
     {
 
-        ToggleInventory();
-        if (Input.GetKeyDown(KeyCode.F))
+        List<Item> item;
+        public int Range;
+        public GameObject cellContainer;
+        public KeyCode showInventory;
+        public int present = 0;
+
+        void Start()
         {
-            Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width/2,Screen.height/2));
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Range))
+            item = new List<Item>();
+
+            cellContainer.SetActive(false);
+
+            for (int i = 0; i < cellContainer.transform.childCount; i++)
             {
-                if (hit.collider.GetComponent<Item>())
+                item.Add(new Item());
+            }
+        }
+
+        void Update()
+        {
+
+            ToggleInventory();
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, Range))
                 {
-                    AddItem(hit.collider.GetComponent<Item>());
+                    if (hit.collider.GetComponent<Item>())
+                    {
+                        AddItem(hit.collider.GetComponent<Item>());
+                    }
                 }
             }
         }
-    }
 
-    void AddItem(Item currentItem)
-    {
-        if (currentItem.isStackable)
+        void AddItem(Item currentItem)
         {
-            AddStackableItem(currentItem);
+            if (currentItem.isStackable)
+            {
+                AddStackableItem(currentItem);
+                present += 1;
+            }
+            else
+            {
+                AddUnstackableItem(currentItem);
+            }
         }
-        else
+
+        void AddUnstackableItem(Item currentItem)
         {
+            for (int i = 0; i < item.Count; i++)
+            {
+                if (item[i].id == 0)
+                {
+                    item[i] = currentItem;
+                    item[i].countItem = 1;
+                    DisplayItems();
+                    Destroy(currentItem.gameObject);
+                    break;
+                }
+            }
+        }
+
+        void AddStackableItem(Item currentItem)
+        {
+            for (int i = 0; i < item.Count; i++)
+            {
+                if (item[i].id == currentItem.id)
+                {
+                    item[i].countItem++;
+                    DisplayItems();
+                    Destroy(currentItem.gameObject);
+                    return;
+                }
+            }
             AddUnstackableItem(currentItem);
         }
-    }
 
-    void AddUnstackableItem(Item currentItem)
-    {
-        for (int i = 0; i < item.Count; i++)
+        void ToggleInventory()
         {
-            if (item[i].id == 0)
+
+            if (Input.GetKeyDown(showInventory))
             {
-                item[i] = currentItem;
-                item[i].countItem = 1;
-                DisplayItems();
-                Destroy(currentItem.gameObject);
-                break;
-            }
-        }
-    }
-
-    void AddStackableItem(Item currentItem)
-    {
-        for (int i = 0; i < item.Count; i++)
-        {
-            if (item[i].id == currentItem.id)
-            {
-                item[i].countItem++;
-                DisplayItems();
-                Destroy(currentItem.gameObject);
-                return;
-            }
-        }
-        AddUnstackableItem(currentItem);
-    }
-
-    void ToggleInventory()
-    {
-
-        if (Input.GetKeyDown(showInventory))
-        {
-            if (cellContainer.activeSelf)
-            {
-                cellContainer.SetActive(false);
-            }
-            else
-            {
-                cellContainer.SetActive(true);
-
-            }
-        }
-    }
-
-    void DisplayItems()
-    {
-        for (int i = 0; i < item.Count; i++)
-        {
-            Transform cell = cellContainer.transform.GetChild(i);
-            Transform icon = cell.GetChild(0);
-            Transform count = icon.GetChild(0);
-
-            Text txt = count.GetComponent<Text>();
-            Image img = icon.GetComponent<Image>();
-            if (item[i].id != 0)
-            {
-
-                img.enabled = true;
-                img.sprite = Resources.Load<Sprite>(item[i].pathIcon);
-                if (item[i].countItem > 1)
+                if (cellContainer.activeSelf)
                 {
-                    txt.text = item[i].countItem.ToString();
+                    cellContainer.SetActive(false);
+                }
+                else
+                {
+                    cellContainer.SetActive(true);
+
                 }
             }
-            else
+        }
+
+        void DisplayItems()
+        {
+            for (int i = 0; i < item.Count; i++)
             {
-                img.enabled = false;
-                img.sprite = null;
-                txt.text = null;
+                Transform cell = cellContainer.transform.GetChild(i);
+                Transform icon = cell.GetChild(0);
+                Transform count = icon.GetChild(0);
+
+                Text txt = count.GetComponent<Text>();
+                Image img = icon.GetComponent<Image>();
+                if (item[i].id != 0)
+                {
+
+                    img.enabled = true;
+                    img.sprite = Resources.Load<Sprite>(item[i].pathIcon);
+                    if (item[i].countItem > 1)
+                    {
+                        txt.text = item[i].countItem.ToString();
+                    }
+                }
+                else
+                {
+                    img.enabled = false;
+                    img.sprite = null;
+                    txt.text = null;
+                }
             }
         }
     }
