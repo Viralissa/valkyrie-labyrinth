ff01453f69170c4f39ce12a6413b34a10e32b089
SinScorpion <sinscorpion90@gmail.com>
2021-05-09 13:31:20 +0700

    Door, button, box
    кнопка активируется ящиком - открывается дверь и закрывается после отжатия

---
+++ Assets/PressButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressButton : MonoBehaviour
+{
+    public GameObject door;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = door.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Box")
+        {
+            _animator.SetBool("OpenDoor", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _animator.SetBool("OpenDoor", false);
+    }
+}
